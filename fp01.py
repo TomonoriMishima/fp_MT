ee1159b335bc3acf859abcf5e4469da38afaad3f
TomonoriMishima <111008257+TomonoriMishima@users.noreply.github.com>
2022-08-11 03:18:08 +0900

    Create fp01.py

---
+++ fp01.py
@@ -0,0 +1,9 @@
+import csv	#csvモジュールのインポート
+
+with open ('/Users/tomo_/Desktop/fixpoint/log01.csv','r') as f:	#csvファイルの読み込み
+	reader = csv.reader(f)		#ファイルからデータを読み込む
+	line = [row for row in reader]		#csvファイルを二次元リストに格納
+	
+	for n in range(len(line)):	#csvファイルの行の数だけループ
+		if line[n][2] == '-':		#応答結果が'-'の場合
+			print(line[n][0] + "~" + line[n+1][0])	#期間の表示
