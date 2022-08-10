cb017546d20a94a39c031908380d662b6d716a80
TomonoriMishima <111008257+TomonoriMishima@users.noreply.github.com>
2022-08-11 03:20:38 +0900

    Update fp01.py

--- fp01.py
+++ fp01.py
@@ -7,4 +7,4 @@ with open ('log01.csv','r') as f:	#csvファイルの読み込み
 	for n in range(len(line)):	#csvファイルの行の数だけループ
 		if line[n][2] == '-':		#応答結果が'-'の場合
 			print(line[n][0] + "~" + line[n+1][0])	#期間の表示
-			print(line[n][1])
+			print(line[n][1])			#アドレスの表示
